290f0ed618ebbb9cb16e5bb68c27d511d9d7a44d
igor n. boulliev <igor@whs.su>
2016-02-14 00:22:46 +0300

    added LocaleDetector (automation.go) with Parse(layout,value) method to parse time with known layout, but unknown locale
         usage:
           var TimeLocaleDetector *monday.LocaleDetector = monday.NewLocaleDetector()
           t, err := TimeLocaleDetector.Parse(layout, value)

---
+++ automation.go
@@ -0,0 +1,90 @@
+package monday
+
+import (
+	"gopkg.in/fatih/set.v0"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var wordsRx = regexp.MustCompile("(\\p{L}+)")
+
+type LocaleDetector struct {
+	localeMap  map[string]*set.Set
+	lastLocale Locale
+}
+
+func (this *LocaleDetector) addWords(words []string, v Locale) {
+	for _, w := range words {
+		l := strings.ToLower(w)
+		if _, ok := this.localeMap[w]; !ok {
+			this.localeMap[w] = set.New(v)
+			if l != w {
+				this.localeMap[l] = set.New(v)
+			}
+		} else {
+			this.localeMap[w].Add(v)
+			if l != w {
+				this.localeMap[l].Add(v)
+			}
+		}
+	}
+}
+
+func NewLocaleDetector() *LocaleDetector {
+	this := &LocaleDetector{localeMap: make(map[string]*set.Set)}
+	for _, v := range ListLocales() {
+		days := GetShortDays(v)
+		this.addWords(days, v)
+		days = GetLongDays(v)
+		this.addWords(days, v)
+		months := GetShortMonths(v)
+		this.addWords(months, v)
+		months = GetLongMonths(v)
+		this.addWords(months, v)
+	}
+	return this
+}
+
+func (this *LocaleDetector) Parse(layout, value string) (time.Time, error) {
+	if this.lastLocale != "" {
+		this.lastLocale = this.detectLocale(value)
+	} else {
+		t, e := ParseInLocation(layout, value, time.UTC, this.lastLocale)
+		if e == nil {
+			return t, nil
+		}
+	}
+	this.lastLocale = this.detectLocale(value)
+	return ParseInLocation(layout, value, time.UTC, this.lastLocale)
+}
+
+func (this *LocaleDetector) detectLocale(value string) Locale {
+	var localesMap map[Locale]int = make(map[Locale]int)
+	for _, v := range wordsRx.FindAllStringSubmatchIndex(value, -1) {
+		word := strings.ToLower(value[v[0]:v[1]])
+		// fmt.Printf("--word:'%s'\n", word)
+		if localesSet, ok := this.localeMap[word]; ok {
+			localesSet.Each(func(i interface{}) bool {
+				if loc, ok := i.(Locale); ok {
+					//      fmt.Printf("\tinc %s\n", loc)
+					if _, ok := localesMap[loc]; !ok {
+						localesMap[loc] = 1
+					} else {
+						localesMap[loc]++
+					}
+				}
+				return true
+			})
+		}
+	}
+	var result Locale = LocaleEnUS
+	var frequency int = 0
+	for key, counter := range localesMap {
+		if counter > frequency {
+			counter = frequency
+			result = key
+		}
+	}
+	return result
+}
